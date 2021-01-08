a66d1aeff0c42924225faa8e38e5f69aa3637a58
sid <sidd.bidaliya2000@gmail.com>
2021-01-08 13:06:52 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyApQYioRoijYxGRH3tbuyTlIfP5Iuvd9ow',
+  authDomain: 'chat-web-app-a602a.firebaseapp.com',
+  projectId: 'chat-web-app-a602a',
+  storageBucket: 'chat-web-app-a602a.appspot.com',
+  messagingSenderId: '606514146208',
+  appId: '1:606514146208:web:7221cb34e1b156a3f912c5',
+};
+
+const app = firebase.initializeApp(config);
