bf6a11db2f7a024e7a277b74a6c6c4481c945bc9
sid <sidd.bidaliya2000@gmail.com>
2021-01-08 15:29:51 +0530

    added sign in page and signed in with google/facebook

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyApQYioRoijYxGRH3tbuyTlIfP5Iuvd9ow',
@@ -10,3 +12,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
