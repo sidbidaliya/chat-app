ac6b70c8e70363bfd122266c0999f5a81fc6d5f8
sid <sidd.bidaliya2000@gmail.com>
2021-01-12 23:42:09 +0530

    started to build chat top; created room description modal

---
+++ src/components/chat-window/top/RoomInfoBtnModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Modal } from 'rsuite';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { useModalState } from '../../../misc/custom-hooks';
+
+const RoomInfoBtnModal = () => {
+  const name = useCurrentRoom(v => v.name);
+  const description = useCurrentRoom(v => v.description);
+
+  const { isOpen, open, close } = useModalState();
+
+  return (
+    <>
+      <Button appearance="link" className="px-0" onClick={open}>
+        Rooom Information
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>About {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h6 className="mb-1">Description</h6>
+          <p>{description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onHide={close} onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default RoomInfoBtnModal;
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,10 +1,39 @@
 import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const Top = () => {
   const name = useCurrentRoom(v => v.name);
+  const isMoblile = useMediaQuery('(max-width: 992px)');
 
-  return <div>{name}</div>;
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon
+            icon="arrow-circle-left"
+            componentClass={Link}
+            to="/"
+            size="2x"
+            className={
+              isMoblile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span className="text-disappear">{name}</span>
+        </h4>
+        <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+      </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
 export default memo(Top);
