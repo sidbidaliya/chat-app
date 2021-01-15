8fb117eb7f9e69ee49a7655ba3c7aefa6382fff4
sid <sidd.bidaliya2000@gmail.com>
2021-01-15 17:28:08 +0530

    add realtime presence listner for a user

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,17 +1,30 @@
 import React, { useCallback } from 'react';
 import { Alert, Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
+import { isOfflineForDatabase } from '../../context/profile.context';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px)');
 
   const onSignOut = useCallback(() => {
-    auth.signOut();
-    Alert.info('Signed Out', 4000);
-    close();
+    // If we are currently connected, then use the 'onDisconnect()'
+    // method to add a set which will only trigger once this
+    // client has disconnected by closing the app,
+    // losing internet, or any other means.
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        Alert.info('Signed Out', 4000);
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
   }, [close]);
 
   return (
