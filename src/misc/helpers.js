59f0362a1ea121e537914f113177c8eb7a9b24b1
sid <sidd.bidaliya2000@gmail.com>
2021-01-18 21:32:15 +0530

    grouped messages by dates

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -45,3 +45,17 @@ export async function getUserUpdates(userId, keyToUpdate, value, db) {
 
   return updates;
 }
+
+export function groupBy(array, groupingKeyFn) {
+  return array.reduce((result, item) => {
+    const groupingKey = groupingKeyFn(item);
+    if (!result[groupingKey]) {
+      // eslint-disable-next-line no-param-reassign
+      result[groupingKey] = [];
+    }
+
+    result[groupingKey].push(item);
+
+    return result;
+  }, {});
+}
